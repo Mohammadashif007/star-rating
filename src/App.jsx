3c559f71d625b95a4dabe7820149312935ec235e
Mohammad Ashif <Mohammad Ashif>
2024-02-14 07:52:36 +0600

    star rating created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import StarRating from "./component";
+
+function App() {
+    return (
+        <>
+            <StarRating noOfStars={10}></StarRating>
+        </>
+    );
+}
+
+export default App;
